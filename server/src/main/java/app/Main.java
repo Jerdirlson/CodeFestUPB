cc24867c3ce2e302b5ce304aa7bd2c2ee3f123a2
Dxckrr <juandavidp1127@gmail.com>
2023-10-25 10:07:49 -0500

    creando estructura
    adding structure

--- server/src/main/java/app/Main.java
+++ server/src/main/java/app/Main.java
@@ -1,7 +1,16 @@
 package app;
 
-public class Main {
+import controller.StartController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        StartController start = new StartController();
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
 }
